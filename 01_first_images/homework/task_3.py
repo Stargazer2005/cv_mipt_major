9ed3e9af1985b303bdc375e7ccb9316d83c480aa
Ilya_Rybalkin <rybalkin.id@phystech.edu>
2025-02-16 03:57:48 +0300

    homework: task_3.1 done

--- 01_first_images/homework/task_3.py
+++ 01_first_images/homework/task_3.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-def rotate(image, point: tuple, angle: float) -> np.ndarray:
+def Rotate(image: np.ndarray, point: tuple, angle: float) -> np.ndarray:
     """
     Повернуть изображение по часовой стрелке на угол от 0 до 360 градусов и преобразовать размер изображения.
 
@@ -11,8 +11,31 @@ def rotate(image, point: tuple, angle: float) -> np.ndarray:
     :param angle: угол поворота
     :return: повернутное изображение
     """
-    # Ваш код
-    pass
+    angle = angle % 360
+
+    while angle > 90:
+        image = Rotate(image, point, 90)
+        angle -= 90
+
+    height, width, _ = image.shape
+
+    rad_angle = np.deg2rad(angle)
+
+    new_height = int(np.ceil(height * np.cos(rad_angle) + width * np.sin(rad_angle)))
+    new_width = int(np.ceil(width * np.cos(rad_angle) + height * np.sin(rad_angle)))
+
+    pts1 = np.float32([[0, 0], [0, height], [width, 0]])
+    pts2 = np.float32(
+        [
+            [0, width * np.sin(rad_angle)],
+            [height * np.sin(rad_angle), new_height],
+            [new_width - height * np.sin(rad_angle), 0],
+        ]
+    )
+
+    M = cv2.getAffineTransform(pts1, pts2)
+
+    image = cv2.warpAffine(image, M, (new_width, new_height))
 
     return image
 
